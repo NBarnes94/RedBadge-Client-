9a3f1933ea81278e7e228ffac16e2efdb570a709
Nick Barnes <Nicholas.Barnes4194@.com>
2021-07-08 15:38:45 -0500

    first heroku deployed attempt

--- src/Components/helpers/environment.js
+++ src/Components/helpers/environment.js
@@ -1,3 +1,10 @@
-let APIUrl = "http://nab-media-server.herokuapp.com";
+let APIUrl = "";
 
+    switch(window.location.hostname){
+        case 'localhost' || '127.0.0.1':
+            APIUrl = "http://localhost:3005";
+            break;
+        case 'nab-media-server.herokuapp.com':
+            APIUrl = 'http://nab-media-server.herokuapp.com'
+    }
 export default APIUrl;

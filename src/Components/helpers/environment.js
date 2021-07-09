2d97dad72a7e46a52d5c93997d011923792a139f
Nick Barnes <Nicholas.Barnes4194@.com>
2021-07-09 09:20:08 -0500

    pushing with apiurl fixed

--- src/Components/helpers/environment.js
+++ src/Components/helpers/environment.js
@@ -1,11 +1,11 @@
-let APIUrl = "";
-// http://nab-media-server.herokuapp.com
+let APIUrl = "http://nab-media-server.herokuapp.com";
 
-    switch(window.location.hostname){
-        case 'localhost' || '127.0.0.1':
-            APIUrl = "http://localhost:3005";
-            break;
-        case 'nab-media-server.herokuapp.com':
-            APIUrl = 'http://nab-media-server.herokuapp.com'
-    }
+
+    // switch(window.location.hostname){
+    //     case 'localhost' || '127.0.0.1':
+    //         APIUrl = "http://localhost:3005";
+    //         break;
+    //     case 'nab-media-server.herokuapp.com':
+    //         APIUrl = 'http://nab-media-server.herokuapp.com'
+    // }
 export default APIUrl;
